38951e8938cecdb2a17c8cc95a7e39fb944ef8fa
MoT186 <tommachefert78240@gmail.com>
2026-04-18 17:59:12 +0200

    Refactor matrix functions and update main logic

--- matrice.c
+++ matrice.c
@@ -1,121 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-void free_matrice(float** matrice,int taille){
-	if(matrice == NULL) return;
-	if(taille <1){
-		printf("Mauvaise taille\n");
-		return;
-	}
-	
-	for(int a=0;a<taille;a++) free(matrice[a]);
-	free(matrice);
-}
-
-float** creation_matrice(int taille){
-
-	if(taille<1){
-		printf("La taille doit être >= 1\n");
-		return NULL;
-	}
-	
-	float** matrice=NULL;
-	matrice=malloc(taille*sizeof(float*));
-	if(matrice == NULL){
-		printf("Erreur allocation mémoire\n");
-		return NULL;
-	}
-	
-	for(int k=0; k<taille; k++){
-		matrice[k]=calloc(taille,sizeof(float));
-		if(matrice[k]==NULL){
-			printf("Erreur allocation mémoire\n");
-			for (int i = 0; i < k; i++) free(matrice[i]);
-            		free(matrice);
-			return NULL;
-		}
-	}
-	
-	return matrice;
-}
-
-float** remplissage(float** matrice, int taille){
-	for(int a=0; a<taille; a++){
-		for(int b=0; b<taille; b++){
-			if(scanf("%f",&matrice[a][b])!=1){
-				printf("La matrice ne doit contenir que des nombres réelles\n");
-				for (int i = 0; i < a; i++) free(matrice[i]);
-            			free(matrice);
-            			return NULL;
-			}
-		}
-	}
-	return matrice;
-}
-
-void affichage_matrice(float** matrice,int taille){
-
-	if(matrice==NULL || taille<1){
-		printf("Impossible d'afficher cette matrice\n");
-		return;
-	}
-
-	for(int a=0; a<taille; a++){
-		printf("| ");
-		for(int b=0; b<taille; b++){
-			printf("%f ",matrice[a][b]);
-		}
-		printf("|\n");
-	}
-}
-
-float* colonne(float** matrice, int numero, int taille){
-	
-	if(matrice==NULL) return NULL;
-	
-	float* col=malloc(taille*sizeof(float));
-	if(col==NULL){
-		printf("Erreur d'allocation mémoire\n");
-		exit(1);
-	}	
-	
-	for(int lig=0;lig<taille;lig++) col[lig]=matrice[lig][numero];
-	return col;
-}
-
-float produit_lig_col(float* ligne, float* colonne, int taille){
-	float somme=0;
-	for(int a=0; a<taille; a++){
-		somme+= ligne[a]*colonne[a];
-	}
-	return somme;
-}
-
-
-
-float** produit_matrice(float** A, float** B, int taille){
-
-	float** result=creation_matrice(taille);
-	float* ligne=malloc(taille*sizeof(float));
-	float* col=NULL;
-	if(ligne == NULL){
-		printf("Erreur allocation mémoire\n");
-		return NULL;
-	}
-	for(int lig=0;lig<taille;lig++){
-	
-		ligne=A[lig];
-		for(int l=0;l<taille;l++){
-			col=colonne(B,l,taille);
-			result[lig][l]=produit_lig_col(ligne,col,taille);
-			free(col);
-		}
-
-	}
-	
-	return result;
-}
+#include "fonction_matrice.h"
+#include "operation_matrice.h"
 
 int main(){
 
@@ -132,13 +18,13 @@ int main(){
 	
 	
 	A=creation_matrice(taille);
-	B=creation_matrice(taille);
+	B=identite(taille,3);
 	
 	printf("Remplissage de A:\n");
 	A=remplissage(A,taille);
 
-	printf("Remplissage de B:\n");
-	B=remplissage(B,taille);
+	/*printf("Remplissage de B:\n");
+	B=remplissage(B,taille);*/
 	
 	C=produit_matrice(A,B,taille);
 
